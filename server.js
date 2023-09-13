fd5d17a4ef32aafbd6ede3bb61a25ea5fd3b9539
RaphMt <rafa.mtorres117@outlook.com>
2023-09-13 17:24:36 -0300

    local storage and sesarch request

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-// import {createServer} from 'node:http'
-
-// const server = createServer((request, response)=> {
-//     response.write('uywyywriy')
-
-//     return response.end()
-// })
-
-// server.listen(9999)
-
 import {fastify} from 'fastify'
+import {databaseMemory} from './database-memory.js'
 
 const server = fastify()
+const database = new databaseMemory()
 
-server.get('/', () => {
-    return 'Hello, World!'
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
 })
+server.get('/videos', (request, reply) => {
+    const search = request.query.search
 
-server.get('/hello', () => {
-    const user = process.env.USER
-    return `Hello, ${user}!`
+    const videos = database.list(search)
+
+    return videos
+})
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    const video = database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
